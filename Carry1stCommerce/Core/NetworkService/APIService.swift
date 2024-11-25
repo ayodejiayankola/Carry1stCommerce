1061b969dfcb4b64544e591704a8d47878969c8b
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-25 19:34:50 +0100

    Implement LiveAPIService and MockAPIService for product bundles

--- Carry1stCommerce/Core/NetworkService/APIService.swift
+++ Carry1stCommerce/Core/NetworkService/APIService.swift
@@ -1,8 +1,32 @@
-//
-//  APIService.swift
-//  Carry1stCommerce
-//
-//  Created by Ayodeji Ayankola on 25/11/2024.
-//
-
 import Foundation
+
+protocol APIServiceable {
+	func getProductBundles() async -> Result<[Product], RequestError>
+}
+
+struct LiveAPIService: APIServiceable, HTTPClient {
+	let urlSession: URLSession
+	
+	init(urlSession: URLSession = .shared) {
+		self.urlSession = urlSession
+	}
+	
+	func getProductBundles() async -> Result<[Product], RequestError> {
+		await request(APIEndpoint.productBundles, responseType: [Product].self)
+	}
+}
+
+#if DEBUG
+// MARK: - Mock API Service
+struct MockAPIService: APIServiceable {
+	let product: [Product]
+	
+	init(product: [Product]) {
+		self.product = product
+	}
+	
+	func getProductBundles() async -> Result<[Product], RequestError> {
+		return .success(product)
+	}
+}
+#endif
