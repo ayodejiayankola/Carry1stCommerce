b39a19c4fc526738917306dcf3eafe58bd4e30d4
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-25 19:36:36 +0100

    Define API endpoints for product bundles

--- Carry1stCommerce/Core/NetworkService/Endpoints.swift
+++ Carry1stCommerce/Core/NetworkService/Endpoints.swift
@@ -1,8 +1,34 @@
-//
-//  Endpoints.swift
-//  Carry1stCommerce
-//
-//  Created by Ayodeji Ayankola on 25/11/2024.
-//
-
 import Foundation
+
+enum APIEndpoint {
+	case productBundles
+}
+
+extension APIEndpoint: Endpoint {
+	var host: String {
+		return "my-json-server.typicode.com"
+	}
+	
+	var path: String {
+		switch self {
+		case .productBundles:
+			return "/carry1stdeveloper/mock-product-api/productBundles"
+		}
+	}
+	
+	var method: HTTPMethod {
+		return .get
+	}
+	
+	var header: [String: String]? {
+		return ["Accept": "application/json"]
+	}
+	
+	var body: [String: Any]? {
+		return nil
+	}
+	
+	var queryParameters: [URLQueryItem]? {
+		return nil
+	}
+}
