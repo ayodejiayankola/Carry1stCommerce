260ec6dcdc2ae4d3e5a031bdfadc243277aa10d5
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-25 19:30:24 +0100

    Implement ImageLoader with caching and async image fetching

--- Carry1stCommerce/Core/NetworkService/ImageLoader.swift
+++ Carry1stCommerce/Core/NetworkService/ImageLoader.swift
@@ -1,8 +1,38 @@
-//
-//  ImageLoader.swift
-//  Carry1stCommerce
-//
-//  Created by Ayodeji Ayankola on 25/11/2024.
-//
+import UIKit
 
-import Foundation
+enum ImageLoaderError: Error {
+	case badRequest
+	case unsupportedImage
+}
+
+@MainActor
+class ImageLoader: ObservableObject {
+	@Published var uiImage: UIImage?
+	private static let cache = NSCache<NSString, UIImage>()
+	
+	func fetchImage(_ url: URL) async throws {
+		let request = URLRequest(url: url)
+		
+		if let cachedImage = Self.cache.object(forKey: url.absoluteString as NSString) {
+			uiImage = cachedImage
+		} else {
+			do {
+				let (data, response) = try await URLSession.shared.data(for: request)
+				guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+					throw ImageLoaderError.badRequest
+				}
+				
+				guard let image = UIImage(data: data) else {
+					throw ImageLoaderError.unsupportedImage
+				}
+				
+				Self.cache.setObject(image, forKey: url.absoluteString as NSString)
+				uiImage = image
+			} catch {
+				
+				print("Error loading image: \(error.localizedDescription)")
+				throw error
+			}
+		}
+	}
+}
