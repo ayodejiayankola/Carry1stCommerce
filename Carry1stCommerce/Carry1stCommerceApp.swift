f03716570724ffa30ebf518fccaffa441daea64a
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-25 15:06:21 +0100

    first commit

---
+++ Carry1stCommerce/Carry1stCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carry1stCommerceApp.swift
+//  Carry1stCommerce
+//
+//  Created by Ayodeji Ayankola on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Carry1stCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
