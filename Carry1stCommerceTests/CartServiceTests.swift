e1bdc8e5538c8e4294f8ebe81059911c1beb3e7c
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-29 12:29:52 +0100

    Task: Add unit tests for CartService methods, including quantity updates and removing non-existing items.

--- Carry1stCommerceTests/CartServiceTests.swift
+++ Carry1stCommerceTests/CartServiceTests.swift
@@ -1,35 +1,100 @@
-//
-//  CartServiceTests.swift
-//  Carry1stCommerceTests
-//
-//  Created by Ayodeji Ayankola on 27/11/2024.
-//
-
 import XCTest
+@testable import Carry1stCommerce
 
-final class CartServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+class CartServiceTests: XCTestCase {
+	var cartService: CartService!
+	
+	override func setUp() {
+		super.setUp()
+		cartService = CartService()
+	}
+	
+	override func tearDown() {
+		cartService = nil
+		super.tearDown()
+	}
+	
+	func testInitialState() {
+		XCTAssertTrue(cartService.items.isEmpty)
+	}
+	
+	func testAddNewProduct() {
+		let product = Product.lives10
+		cartService.add(product: product)
+		
+		XCTAssertEqual(cartService.items.count, 1)
+		XCTAssertEqual(cartService.items.first?.quantity, 1)
+	}
+	
+	func testAddExistingProduct() {
+		let product = Product.credits1250
+		cartService.add(product: product)
+		cartService.add(product: product)
+		
+		XCTAssertEqual(cartService.items.count, 1)
+		XCTAssertEqual(cartService.items.first?.quantity, 2)
+	}
+	
+	func testRemoveProduct() {
+		let product = Product.credits150
+		cartService.add(product: product)
+		
+		let cartItem = cartService.items.first!
+		cartService.remove(cartItem: cartItem)
+		
+		XCTAssertTrue(cartService.items.isEmpty)
+	}
+	
+	
+	func testUpdateQuantity() {
+		let product = Product.credits5000
+		cartService.add(product: product)
+		
+		let cartItem = cartService.items.first!
+		cartService.update(cartItem: cartItem, quantity: 3)
+		
+		XCTAssertEqual(cartService.items.first?.quantity, 3)
+	}
+	
+	func testUpdateQuantityToZero() {
+		let product = Product.credits1250
+		cartService.add(product: product)
+		
+		let cartItem = cartService.items.first!
+		cartService.update(cartItem: cartItem, quantity: 0)
+		
+		XCTAssertTrue(cartService.items.isEmpty)
+	}
+	
+	func testTotalItems() {
+		let product1 = Product.lives25
+		let product2 = Product.credits1250
+		
+		cartService.add(product: product1)
+		cartService.add(product: product1)
+		cartService.add(product: product2)
+		
+		XCTAssertEqual(cartService.totalItems(), 3)
+	}
+	
+	
+	func testTotalPrice() {
+		let product1 = Product.credits1250
+		let product2 = Product.credits1250
+		
+		cartService.add(product: product1)
+		cartService.add(product: product1)
+		cartService.add(product: product2)
+		
+		XCTAssertEqual(cartService.totalPrice(), 15.0)
+	}
+	
+	func testItemCount() {
+		let product = Product.credits1250
+		cartService.add(product: product)
+		cartService.add(product: product)
+		
+		XCTAssertEqual(cartService.itemCount(for: product), 2)
+	}
 }
+
