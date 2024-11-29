536ff68ae02e268dead4983e3a4f43a7f84e49c5
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-29 12:43:12 +0100

    Task: Improve CartBadgeView with dynamic item count display and animation

--- Carry1stCommerce/Features/Cart/View/CartBadgeView.swift
+++ Carry1stCommerce/Features/Cart/View/CartBadgeView.swift
@@ -2,12 +2,12 @@ import SwiftUI
 
 struct CartBadgeView: View {
 	var itemCount: Int
-	
+
 	private let badgeSize: CGFloat = 18
 	private let fontSize: CGFloat = 12
 	private let cartIconSize: CGFloat = 20
 	private let badgeOffset: CGSize = CGSize(width: 12, height: -10)
-	
+
 	var body: some View {
 		ZStack(alignment: .topTrailing) {
 			Image(systemName: "cart")
@@ -16,7 +16,7 @@ struct CartBadgeView: View {
 				.frame(width: cartIconSize, height: cartIconSize)
 				.symbolVariant(itemCount > 0 ? .fill : .none)
 				.symbolEffect(.bounce, value: itemCount)
-			
+
 			if itemCount > 0 {
 				Text("\(itemCount)")
 					.font(.system(size: fontSize, weight: .bold))
@@ -33,6 +33,7 @@ struct CartBadgeView: View {
 		}
 	}
 }
+
 
 #if DEBUG
 #Preview {
