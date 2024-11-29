0eb849ff231d5ddceeb34057cd233deb088c1e36
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-29 11:59:51 +0100

    Task: Implement unit tests for LiveAPIService: Handling successful, unauthorized, invalid, and network error responses for product bundles

--- Carry1stCommerceTests/Mocks/MockURLProtocol.swift
+++ Carry1stCommerceTests/Mocks/MockURLProtocol.swift
@@ -1,8 +1,32 @@
-//
-//  MockURLProtocol.swift
-//  Carry1stCommerceTests
-//
-//  Created by Ayodeji Ayankola on 28/11/2024.
-//
+import XCTest
+@testable import Carry1stCommerce
 
-import Foundation
+class MockURLProtocol: URLProtocol {
+	static var requestHandler: ((URLRequest) throws -> (URLResponse, Data))?
+	
+	override class func canInit(with request: URLRequest) -> Bool {
+		return true
+	}
+	
+	override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+		return request
+	}
+	
+	override func startLoading() {
+		guard let handler = MockURLProtocol.requestHandler else {
+			XCTFail("No request handler set")
+			return
+		}
+		
+		do {
+			let (response, data) = try handler(request)
+			client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+			client?.urlProtocol(self, didLoad: data)
+			client?.urlProtocolDidFinishLoading(self)
+		} catch {
+			client?.urlProtocol(self, didFailWithError: error)
+		}
+	}
+	
+	override func stopLoading() {}
+}
