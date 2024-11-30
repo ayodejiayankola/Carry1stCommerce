de5a8503d7029dbd946aac6aee1fc4042ff9bb6f
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-30 16:49:18 +0100

    Refactor: ProductListView

--- Carry1stCommerce/Features/Product/View/ProductListView.swift
+++ Carry1stCommerce/Features/Product/View/ProductListView.swift
@@ -17,40 +17,74 @@ struct ProductListView: View {
 		NavigationView {
 			Group {
 				if productViewModel.isLoading {
-					ProgressView(AppStrings.ProductList.loadingProducts)
-						.padding()
-						.foregroundColor(Color(uiColor: .label))
-						.background(Color(uiColor: .secondarySystemBackground))
+					loadingView
 				} else if let error = productViewModel.error {
-					Text(error.message)
-						.foregroundColor(.red)
-						.padding()
-						.background(Color(uiColor: .secondarySystemBackground))
+					errorView(error: error)
 				} else {
-					ScrollView {
-						LazyVGrid(columns: gridItems, spacing: 16) {
-							ForEach(productViewModel.products) { product in
-								ProductGridItem(product: product)
-									.padding(.horizontal, 16)
-							}
-						}
-						.padding(.horizontal, 16)
-					}
+					productGrid
 				}
 			}
 			.task {
 				await productViewModel.fetchProducts()
 			}
 			.navigationTitle(AppStrings.ProductList.navigationTitle)
-			.navigationBarItems(trailing:
-									NavigationLink(destination: CartView()) {
-				CartBadgeView(itemCount: cartManager.totalItems())
-					.toolbarRole(.editor)
+			.navigationBarItems(trailing: cartButton)
+		}
+	}
+	
+	// MARK: - Views
+	
+	private var loadingView: some View {
+		ProgressView(AppStrings.ProductList.loadingProducts)
+			.padding()
+			.foregroundColor(Color(uiColor: .label))
+			.background(Color(uiColor: .secondarySystemBackground))
+	}
+	
+	private func errorView(error: AlertData) -> some View {
+		VStack(spacing: 16) {
+			Text(error.message)
+				.foregroundColor(.red)
+				.padding()
+				.background(Color(uiColor: .secondarySystemBackground))
+			
+			PrimaryButton(
+				title: AppStrings.Common.retry,
+				backgroundColor: .clear,
+				textColor: .systemGray,
+				action: {
+					Task {
+						await productViewModel.fetchProducts(forceRefresh: true)
+					}
+				}
+			).foregroundStyle(.bar)
+			
+			
+		}
+	}
+	
+	private var productGrid: some View {
+		ScrollView {
+			LazyVGrid(columns: gridItems, spacing: 16) {
+				ForEach(productViewModel.products) { product in
+					ProductGridItem(product: product)
+				}
 			}
-			)
+			.padding(.horizontal, 16)
+		}
+		.refreshable {
+			await productViewModel.fetchProducts()
+		}
+	}
+	
+	private var cartButton: some View {
+		NavigationLink(destination: CartView()) {
+			CartBadgeView(itemCount: cartManager.totalItems())
+				.toolbarRole(.editor)
 		}
 	}
 }
+
 
 #if DEBUG
 struct ProductListView_Previews: PreviewProvider {
@@ -62,3 +96,6 @@ struct ProductListView_Previews: PreviewProvider {
 	}
 }
 #endif
+
+
+
