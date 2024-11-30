f20ebf6afe306886a4020f1a3b6b76dad18bc67d
Ayodeji Ayankola <ayodejiayankola@gmail.com>
2024-11-30 16:51:00 +0100

    Refactor: PrimaryButton resuable components

--- Carry1stCommerce/Shared/PrimaryButton.swift
+++ Carry1stCommerce/Shared/PrimaryButton.swift
@@ -3,18 +3,22 @@ import SwiftUI
 struct PrimaryButton: View {
 	var title: String
 	var backgroundColor: UIColor
+	var textColor: UIColor = .white
+	var font: Font = .headline
+	var shadowColor: Color = .black.opacity(0.1)
 	var action: () -> Void
 	
 	var body: some View {
 		Button(action: action) {
 			Text(title)
-				.font(.headline)
-				.foregroundColor(.white)
+				.font(font)
+				.foregroundColor(Color(uiColor: textColor))
 				.frame(maxWidth: .infinity)
 				.padding()
 				.background(Color(uiColor: backgroundColor))
 				.cornerRadius(10)
-				.shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
+				.shadow(color: shadowColor, radius: 3, x: 0, y: 2)
 		}
 	}
 }
+
